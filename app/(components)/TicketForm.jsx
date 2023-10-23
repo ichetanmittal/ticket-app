16fb5c09d1050666cdc4f046df4c68b4084b25f0
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2023-10-23 14:38:18 +0530

    Route and component structuring done

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, {useState} from "react";
+
+const TicketForm = () => {
+  return (
+    <div>TicketForm</div>
+  )
+}
+
+export default TicketForm
